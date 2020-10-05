b4e2f0cc83b8b33cf6d12d07a838afcf3cf75ca6
mskian <veerfun23@gmail.com>
2020-10-05 18:16:49 +0530

    Add env for store API and Group ID Update package.json Update Readme add gitignore Add start Command for run the Script

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+require('dotenv').config();
 
 //Set UserVariables(Custom UserAttributes)
 var user_name = event.user.customAttributes.first_name;
@@ -6,23 +7,24 @@ var user_email = event.user.customAttributes.user_email;
 
 //Setting Custom Header and API KEY
 var headers = {
-'Content-Type': 'application/json',
-'X-MailerLite-ApiKey': 'MAILERLITE-API-KEY'
+    'Content-Type': 'application/json',
+    'X-MailerLite-ApiKey': process.env.MAILERLITEAPIKEY
 };
 
 //Actual Data to be Sent
-var dataString = '{"email": "'+user_email+'", "name": "'+user_name+'", "type": "active"}';
+var dataString = '{"email": "' + user_email + '", "name": "' + user_name + '", "type": "active"}';
+var group_id = process.env.GROUPID
 
 var options = {
-url: 'https://api.mailerlite.com/api/v2/groups/GROUP-ID/subscribers',
-method: 'POST',
-headers: headers,
-body: dataString
+    url: 'https://api.mailerlite.com/api/v2/groups/' + group_id + '/subscribers',
+    method: 'POST',
+    headers: headers,
+    body: dataString
 };
 
 function callback(error, response, body) {
-if (!error && response.statusCode == 200) {
-console.log(body);
-}
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
 }
 request(options, callback);
